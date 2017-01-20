58b8833964fce527f78b6eff839d285af4931d07
Eduard Ganiukov <eduard.ganiukov@lovoo.com>
2017-01-20 18:42:20 +0100

    fcm: fix validation issue, when `condition` is set #5

--- message_test.go
+++ message_test.go
@@ -3,11 +3,13 @@ package fcm
 import "testing"
 
 func TestValidate(t *testing.T) {
-	t.Run("validate=to_many_reg_ids", func(t *testing.T) {
+	t.Run("valid with token", func(t *testing.T) {
 		msg := &Message{
-			Token:           "test",
-			RegistrationIDs: []string{"reg_id"},
-			TimeToLive:      3600,
+			Token:      "test",
+			TimeToLive: 3600,
+			Data: map[string]interface{}{
+				"message": "This is a Firebase Cloud Messaging Topic Message!",
+			},
 		}
 		err := msg.Validate()
 		if err != nil {
@@ -15,42 +17,88 @@ func TestValidate(t *testing.T) {
 		}
 	})
 
-	t.Run("validate=invalid_message", func(t *testing.T) {
+	t.Run("invalid message", func(t *testing.T) {
 		var msg *Message
 		err := msg.Validate()
 		if err == nil {
-			t.Fatal("expected 'message is invalid' error but got nil")
+			t.Fatalf("expected <%v> error, but got <nil>", ErrInvalidMessage)
 		}
 	})
 
-	t.Run("validate=invalid_token", func(t *testing.T) {
-		msg := &Message{}
+	t.Run("invalid target", func(t *testing.T) {
+		msg := &Message{
+			Data: map[string]interface{}{
+				"message": "This is a Firebase Cloud Messaging Topic Message!",
+			},
+		}
 		err := msg.Validate()
 		if err == nil {
-			t.Fatal("expected 'device token is invalid' error but got nil")
+			t.Fatalf("expected <%v> error, but got nil", ErrInvalidTarget)
 		}
 	})
 
-	t.Run("validaate=to_many_reg_ids", func(t *testing.T) {
+	t.Run("too many registration ids", func(t *testing.T) {
 		msg := &Message{
 			Token:           "test",
 			RegistrationIDs: make([]string, 2000),
 		}
 		err := msg.Validate()
 		if err == nil {
-			t.Fatal("expected 'too many registrations id' error but got nil")
+			t.Fatalf("expected <%v> error, but got <nil>", ErrToManyRegIDs)
 		}
 	})
 
-	t.Run("validaate=to_many_reg_ids", func(t *testing.T) {
+	t.Run("invalid TTL", func(t *testing.T) {
 		msg := &Message{
 			Token:           "test",
 			RegistrationIDs: []string{"reg_id"},
 			TimeToLive:      2500000,
+			Data: map[string]interface{}{
+				"message": "This is a Firebase Cloud Messaging Topic Message!",
+			},
 		}
 		err := msg.Validate()
 		if err == nil {
-			t.Fatal("expected 'message time-to-live is invali' error but got nil")
+			t.Fatalf("expected <%v> error, but got nil", ErrInvalidTimeToLive)
+		}
+	})
+
+	t.Run("valid with registration ID", func(t *testing.T) {
+		msg := &Message{
+			RegistrationIDs: []string{"reg_id"},
+			Data: map[string]interface{}{
+				"message": "This is a Firebase Cloud Messaging Topic Message!",
+			},
+		}
+		err := msg.Validate()
+		if err != nil {
+			t.Fatalf("unexpected error: %v", err)
+		}
+	})
+
+	t.Run("valid with condition", func(t *testing.T) {
+		msg := &Message{
+			Condition: "'dogs' in topics || 'cats' in topics",
+			Data: map[string]interface{}{
+				"message": "This is a Firebase Cloud Messaging Topic Message!",
+			},
+		}
+		err := msg.Validate()
+		if err != nil {
+			t.Fatalf("unexpected error: %v", err)
+		}
+	})
+
+	t.Run("invalid condition", func(t *testing.T) {
+		msg := &Message{
+			Condition: "'TopicA' in topics && ('TopicB' in topics || 'TopicC' in topics || 'TopicD' in topics )",
+			Data: map[string]interface{}{
+				"message": "This is a Firebase Cloud Messaging Topic Message!",
+			},
+		}
+		err := msg.Validate()
+		if err == nil {
+			t.Fatalf("expected <%v> error, but got nil", ErrInvalidTarget)
 		}
 	})
 }
