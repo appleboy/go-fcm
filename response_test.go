e4803cf47d4f528e6fdaebcc3b37f1227cc45d0b
Bo-Yi Wu <appleboy.tw@gmail.com>
2017-10-24 13:58:18 +0800

    support Topic HTTP response
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- response_test.go
+++ response_test.go
@@ -9,16 +9,16 @@ import (
 func TestUnmarshal(t *testing.T) {
 	t.Run("unmarshal=success", func(t *testing.T) {
 		data := []byte(`{
-			"multicast_id":10,
-			"success": 0,
-			"failure":1,
-			"canonical_ids":10,
-			"results": [{
-				"message_id":"q1w2e3r4",
-				"registration_id": "t5y6u7i8o9",
-				"error": "NotRegistered"
-			}]
-		}`)
+  "multicast_id":10,
+  "success": 0,
+  "failure":1,
+  "canonical_ids":10,
+  "results": [{
+    "message_id":"q1w2e3r4",
+    "registration_id": "t5y6u7i8o9",
+    "error": "NotRegistered"
+  }]
+}`)
 
 		var response Response
 		err := json.Unmarshal(data, &response)
@@ -47,13 +47,13 @@ func TestUnmarshal(t *testing.T) {
 
 	t.Run("Device Group HTTP Response", func(t *testing.T) {
 		data := []byte(`{
-      "success":1,
-      "failure":2,
-      "failed_registration_ids":[
-        "regId1",
-        "regId2"
-      ]
-		}`)
+  "success":1,
+  "failure":2,
+  "failed_registration_ids":[
+    "regId1",
+    "regId2"
+  ]
+}`)
 
 		var response Response
 		err := json.Unmarshal(data, &response)
@@ -73,18 +73,38 @@ func TestUnmarshal(t *testing.T) {
 		}
 	})
 
+	t.Run("Topic HTTP response", func(t *testing.T) {
+		data := []byte(`{
+  "message_id": "1023456",
+  "error": "TopicsMessageRateExceeded"
+}`)
+
+		var response Response
+		err := json.Unmarshal(data, &response)
+		if err != nil {
+			t.Fatalf("unexpected error: %v", err)
+		}
+		expected := Response{
+			MessageID: "1023456",
+			Error:     errMap["TopicsMessageRateExceeded"],
+		}
+		if !reflect.DeepEqual(response, expected) {
+			t.Fatalf("expected: %v\ngot: %v", expected, response)
+		}
+	})
+
 	t.Run("unmarshal=success_timeout", func(t *testing.T) {
 		data := []byte(`{
-			"multicast_id":10,
-			"success": 0,
-			"failure":1,
-			"canonical_ids":10,
-			"results": [{
-				"message_id":"q1w2e3r4",
-				"registration_id": "t5y6u7i8o9",
-				"error": "Unavailable"
-			}]
-		}`)
+  "multicast_id":10,
+  "success": 0,
+  "failure":1,
+  "canonical_ids":10,
+  "results": [{
+    "message_id":"q1w2e3r4",
+    "registration_id": "t5y6u7i8o9",
+    "error": "Unavailable"
+  }]
+}`)
 
 		var response Response
 		err := json.Unmarshal(data, &response)
@@ -99,16 +119,16 @@ func TestUnmarshal(t *testing.T) {
 
 	t.Run("unmarshal=failed", func(t *testing.T) {
 		data := []byte(`{
-			"multicast_id":10,
-			"success": 0,
-			"failure":1,
-			"canonical_ids":10,
-			"results": [{
-				"message_id":["q1w2e3r4"],
-				"registration_id": "t5y6u7i8o9",
-				"error": "NotRegistered"
-			}]
-		}`)
+  "multicast_id":10,
+  "success": 0,
+  "failure":1,
+  "canonical_ids":10,
+  "results": [{
+    "message_id":["q1w2e3r4"],
+    "registration_id": "t5y6u7i8o9",
+    "error": "NotRegistered"
+  }]
+}`)
 
 		var response Response
 		err := json.Unmarshal(data, &response)
