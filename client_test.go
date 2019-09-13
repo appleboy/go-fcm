105acc776544633e40d56e31d14b8fd90d675a3c
Dima <hummerd@mail.ru>
2019-09-13 22:27:59 +0300

    add tests  for send with context

--- client_test.go
+++ client_test.go
@@ -1,6 +1,7 @@
 package fcm
 
 import (
+	"context"
 	"fmt"
 	"net/http"
 	"net/http/httptest"
@@ -9,6 +10,46 @@ import (
 )
 
 func TestSend(t *testing.T) {
+	t.Run("send=success", func(t *testing.T) {
+		server := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+			if req.Header.Get("Authorization") != "key=test" {
+				t.Fatalf("expected: key=test\ngot: %s", req.Header.Get("Authorization"))
+			}
+			rw.WriteHeader(http.StatusOK)
+			rw.Header().Set("Content-Type", "application/json")
+			fmt.Fprint(rw, `{
+				"success": 1,
+				"failure":0,
+				"results": [{
+					"message_id":"q1w2e3r4",
+					"registration_id": "t5y6u7i8o9",
+					"error": ""
+				}]
+			}`)
+		}))
+		defer server.Close()
+
+		client, err := NewClient("test", WithEndpoint(server.URL), WithTimeout(10*time.Second))
+		if err != nil {
+			t.Fatalf("unexpected error: %v", err)
+		}
+		resp, err := client.Send(&Message{
+			To: "test",
+			Data: map[string]interface{}{
+				"foo": "bar",
+			},
+		})
+		if err != nil {
+			t.Fatalf("unexpected error: %v", err)
+		}
+		if resp.Success != 1 {
+			t.Fatalf("expected 1 successes\ngot: %d sucesses", resp.Success)
+		}
+		if resp.Failure != 0 {
+			t.Fatalf("expected 0 failures\ngot: %d failures", resp.Failure)
+		}
+	})
+
 	t.Run("send=success", func(t *testing.T) {
 		server := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
 			if req.Header.Get("Authorization") != "key=test" {
@@ -289,6 +330,93 @@ func TestSendWithRetry(t *testing.T) {
 		}
 		if resp != nil {
 			t.Fatalf("expected nil response\ngot: %v response", resp)
+		}
+	})
+}
+
+func TestSendWithContext(t *testing.T) {
+	t.Run("send_context=success", func(t *testing.T) {
+		server := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+			if req.Header.Get("Authorization") != "key=test" {
+				t.Fatalf("expected: key=test\ngot: %s", req.Header.Get("Authorization"))
+			}
+			rw.WriteHeader(http.StatusOK)
+			rw.Header().Set("Content-Type", "application/json")
+			fmt.Fprint(rw, `{
+				"success": 1,
+				"failure":0,
+				"results": [{
+					"message_id":"q1w2e3r4",
+					"registration_id": "t5y6u7i8o9",
+					"error": ""
+				}]
+			}`)
+		}))
+		defer server.Close()
+
+		client, err := NewClient("test", WithEndpoint(server.URL), WithTimeout(10*time.Second))
+		if err != nil {
+			t.Fatalf("unexpected error: %v", err)
+		}
+
+		ctx := context.Background()
+		resp, err := client.SendWithContext(ctx, &Message{
+			To: "test",
+			Data: map[string]interface{}{
+				"foo": "bar",
+			},
+		})
+		if err != nil {
+			t.Fatalf("unexpected error: %v", err)
+		}
+		if resp.Success != 1 {
+			t.Fatalf("expected 1 successes\ngot: %d sucesses", resp.Success)
+		}
+		if resp.Failure != 0 {
+			t.Fatalf("expected 0 failures\ngot: %d failures", resp.Failure)
+		}
+	})
+
+	t.Run("send_context=timeout", func(t *testing.T) {
+		server := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+			if req.Header.Get("Authorization") != "key=test" {
+				t.Fatalf("expected: key=test\ngot: %s", req.Header.Get("Authorization"))
+			}
+			time.Sleep(time.Millisecond * 100)
+			rw.WriteHeader(http.StatusOK)
+			rw.Header().Set("Content-Type", "application/json")
+			fmt.Fprint(rw, `{
+				"success": 1,
+				"failure":0,
+				"results": [{
+					"message_id":"q1w2e3r4",
+					"registration_id": "t5y6u7i8o9",
+					"error": ""
+				}]
+			}`)
+		}))
+		defer server.Close()
+
+		client, err := NewClient("test", WithEndpoint(server.URL), WithTimeout(10*time.Second))
+		if err != nil {
+			t.Fatalf("unexpected error: %v", err)
+		}
+
+		ctx, cancel := context.WithTimeout(context.Background(), time.Millisecond*50)
+		defer cancel()
+		_, err = client.SendWithContext(ctx, &Message{
+			To: "test",
+			Data: map[string]interface{}{
+				"foo": "bar",
+			},
+		})
+		if err == nil {
+			t.Fatalf("no context timeout")
+		}
+
+		_, ok := err.(connectionError)
+		if !ok {
+			t.Fatalf("error is not fcm.connectionError \ngot: %T", err)
 		}
 	})
 }
