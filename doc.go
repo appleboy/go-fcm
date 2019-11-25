53a05b1d62078d05ab23c75ff78c35a83df1af55
Saddam H <thedevsaddam@gmail.com>
2019-11-25 21:52:13 +0600

    Add godoc example (#20)

---
+++ doc.go
@@ -0,0 +1,28 @@
+// Package fcm provides Firebase Cloud Messaging functionality for Golang
+//
+// Here is a simple example illustrating how to use FCM library:
+// func main() {
+// 	// Create the message to be sent.
+// 	msg := &fcm.Message{
+// 		To: "sample_device_token",
+// 		Data: map[string]interface{}{
+// 			"foo": "bar",
+// 		},
+// 	}
+//
+// 	// Create a FCM client to send the message.
+// 	client, err := fcm.NewClient("sample_api_key")
+// 	if err != nil {
+// 		log.Fatalln(err)
+// 	}
+//
+// 	// Send the message and receive the response without retries.
+// 	response, err := client.Send(msg)
+// 	if err != nil {
+// 		log.Fatalln(err)
+// 	}
+//
+// 	log.Printf("%#v\n", response)
+// }
+//
+package fcm
