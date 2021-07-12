f7ee3b5fc3880c509b39f89e73ec5c963dff45f5
alper <alper@tabak.app>
2021-07-12 10:46:11 +0300

    Build pass commit.

--- example/main.go
+++ example/main.go
@@ -9,18 +9,18 @@ import (
 func main() {
 	// Create the message to be sent.
 	msg := &fcm.Message{
-		To: "sample_device_token",
+		To: "e-udVMWkc0fshHO52uosbh:APA91bFn2WPHd_xZM69QxGL21On6wDZIQKMIXKo5ChBYw-3aiaMnzxQkgTLWXVIJg1U0vuGuNPSyaEJS6rlNckJKrOK5crtmqcLzU_vHOHiTzzIbIgNfpCXH2uR-qJNTICREZSug6YWH",
 		Data: map[string]interface{}{
 			"foo": "bar",
 		},
 		Notification: &fcm.Notification{
 			Title: "title",
-			Body: "body",
+			Body:  "body",
 		},
 	}
 
 	// Create a FCM client to send the message.
-	client, err := fcm.NewClient("sample_api_key")
+	client, err := fcm.NewClient("AAAAr3cjMuo:APA91bGk-e4Oe2FB43yjySy9KoMZk6qo-cY7G-uyrwB6XoV14cMgAEo7jkJyz4t33oovHu4RWyr-HYq4z1voN8N17QdHTnLWPrHxEhYAT1gR4RmDUIMpzilRJ6FDYDNbRW5b-Xtwy2nt")
 	if err != nil {
 		log.Fatalln(err)
 	}
