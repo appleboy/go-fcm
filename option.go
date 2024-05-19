53a864525eede253c333582da1adc8fc0fb83fe4
appleboy <appleboy.tw@gmail.com>
2024-05-19 16:27:14 +0800

    refactor: refactor HTTP client configuration and cleanup code
    - Remove unused imports `errors` and `time`
    - Refactor `WithHTTPClient` to append the option to `c.options`
    - Initialize `httpClient` within `WithHTTPProxy` and append the option to `c.options`
    - Remove `WithTimeout` function
    - Remove `TestWithHTTPProxy` test function
    
    Signed-off-by: appleboy <appleboy.tw@gmail.com>

--- option.go
+++ option.go
@@ -1,10 +1,8 @@
 package fcm
 
 import (
-	"errors"
 	"net/http"
 	"net/url"
-	"time"
 
 	"google.golang.org/api/option"
 )
@@ -15,18 +13,7 @@ type Option func(*Client) error
 // WithHTTPClient returns Option to configure HTTP Client.
 func WithHTTPClient(httpClient *http.Client) Option {
 	return func(c *Client) error {
-		c.httpClient = httpClient
-		return nil
-	}
-}
-
-// WithTimeout returns Option to configure HTTP Client timeout.
-func WithTimeout(d time.Duration) Option {
-	return func(c *Client) error {
-		if d.Nanoseconds() <= 0 {
-			return errors.New("invalid timeout duration")
-		}
-		c.timeout = d
+		c.options = append(c.options, option.WithHTTPClient(httpClient))
 		return nil
 	}
 }
@@ -34,11 +21,13 @@ func WithTimeout(d time.Duration) Option {
 // WithHTTPProxy returns Option to configure HTTP Client proxy.
 func WithHTTPProxy(proxyURL string) Option {
 	return func(c *Client) error {
+		httpClient := http.DefaultClient
 		proxy, err := url.Parse(proxyURL)
 		if err != nil {
 			return err
 		}
-		c.httpClient.Transport.(*http.Transport).Proxy = http.ProxyURL(proxy)
+		httpClient.Transport.(*http.Transport).Proxy = http.ProxyURL(proxy)
+		c.options = append(c.options, option.WithHTTPClient(httpClient))
 		return nil
 	}
 }
