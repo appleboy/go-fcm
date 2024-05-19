e331bfc38093ff6dec506eaec03bc9bd4caa320c
appleboy <appleboy.tw@gmail.com>
2024-05-19 16:26:20 +0800

    feat: refactor Client struct and add new message sending methods
    - Remove unused imports `net/http` and `time`
    - Remove `DefaultTimeout` constant
    - Remove `httpClient` and `timeout` fields from `Client` struct
    - Simplify `NewClient` function by removing `httpClient` and `timeout` initialization
    - Remove commented-out functions related to sending messages and retries
    - Add new `Send` method to `Client` struct for sending messages
    - Add new `SendDryRun` method to `Client` struct for sending messages in dry run mode
    - Add new `SendMulticast` method to `Client` struct for sending multicast messages
    - Add new `SendMulticastDryRun` method to `Client` struct for sending multicast messages in dry run mode
    
    Signed-off-by: appleboy <appleboy.tw@gmail.com>

--- client.go
+++ client.go
@@ -2,17 +2,10 @@ package fcm
 
 import (
 	"context"
-	"net/http"
-	"time"
 
 	firebase "firebase.google.com/go/v4"
 	"firebase.google.com/go/v4/messaging"
 	"google.golang.org/api/option"
-)
-
-const (
-	// DefaultTimeout duration in second
-	DefaultTimeout time.Duration = 30 * time.Second
 )
 
 // Client abstracts the interaction between the application server and the
@@ -23,11 +16,13 @@ const (
 //
 // If the `HTTP` field is nil, a zeroed http.Client will be allocated and used
 // to send messages.
+//
+// Authorization Scopes
+// Requires one of the following OAuth scopes:
+// - https://www.googleapis.com/auth/firebase.messaging
+// - https://www.googleapis.com/auth/cloud-platform
 type Client struct {
-	httpClient *http.Client
-	timeout    time.Duration
-	client     *messaging.Client
-
+	client        *messaging.Client
 	serviceAcount string
 	options       []option.ClientOption
 }
@@ -35,10 +30,7 @@ type Client struct {
 // NewClient creates new Firebase Cloud Messaging Client based on API key and
 // with default endpoint and http client.
 func NewClient(ctx context.Context, opts ...Option) (*Client, error) {
-	c := &Client{
-		httpClient: http.DefaultClient,
-		timeout:    DefaultTimeout,
-	}
+	c := &Client{}
 	for _, o := range opts {
 		if err := o(c); err != nil {
 			return nil, err
@@ -65,104 +57,47 @@ func NewClient(ctx context.Context, opts ...Option) (*Client, error) {
 	return c, nil
 }
 
-// // SendWithContext sends a message to the FCM server without retrying in case of service
-// // unavailability. A non-nil error is returned if a non-recoverable error
-// // occurs (i.e. if the response status is not "200 OK").
-// // Behaves just like regular send, but uses external context.
-// func (c *Client) SendWithContext(ctx context.Context, msg *Message) (*Response, error) {
-// 	// validate
-// 	if err := msg.Validate(); err != nil {
-// 		return nil, err
-// 	}
+// SendWithContext sends a message to the FCM server without retrying in case of service
+// unavailability. A non-nil error is returned if a non-recoverable error
+// occurs (i.e. if the response status is not "200 OK").
+// Behaves just like regular send, but uses external context.
+func (c *Client) Send(ctx context.Context, message ...*messaging.Message) (*messaging.BatchResponse, error) {
+	resp, err := c.client.SendEach(ctx, message)
+	if err != nil {
+		return nil, err
+	}
 
-// 	// marshal message
-// 	data, err := json.Marshal(msg)
-// 	if err != nil {
-// 		return nil, err
-// 	}
+	return resp, nil
+}
 
-// 	return c.send(ctx, data)
-// }
+// SendDryRun sends the messages in the given array via Firebase Cloud Messaging in the
+// dry run (validation only) mode.
+func (c *Client) SendDryRun(ctx context.Context, message ...*messaging.Message) (*messaging.BatchResponse, error) {
+	resp, err := c.client.SendEachDryRun(ctx, message)
+	if err != nil {
+		return nil, err
+	}
 
-// // Send sends a message to the FCM server without retrying in case of service
-// // unavailability. A non-nil error is returned if a non-recoverable error
-// // occurs (i.e. if the response status is not "200 OK").
-// func (c *Client) Send(msg *Message) (*Response, error) {
-// 	ctx, cancel := context.WithTimeout(context.Background(), c.timeout)
-// 	defer cancel()
+	return resp, nil
+}
 
-// 	return c.SendWithContext(ctx, msg)
-// }
+// SendEachForMulticast sends the given multicast message to all the FCM registration tokens specified.
+func (c *Client) SendMulticast(ctx context.Context, message *messaging.MulticastMessage) (*messaging.BatchResponse, error) {
+	resp, err := c.client.SendEachForMulticast(ctx, message)
+	if err != nil {
+		return nil, err
+	}
 
-// // SendWithRetry sends a message to the FCM server with defined number of
-// // retrying in case of temporary error.
-// func (c *Client) SendWithRetry(msg *Message, retryAttempts int) (*Response, error) {
-// 	return c.SendWithRetryWithContext(context.Background(), msg, retryAttempts)
-// }
+	return resp, nil
+}
 
-// // SendWithRetryWithContext sends a message to the FCM server with defined number of
-// // retrying in case of temporary error.
-// // Behaves just like regular SendWithRetry, but uses external context.
-// func (c *Client) SendWithRetryWithContext(ctx context.Context, msg *Message, retryAttempts int) (*Response, error) {
-// 	// validate
-// 	if err := msg.Validate(); err != nil {
-// 		return nil, err
-// 	}
-// 	// marshal message
-// 	data, err := json.Marshal(msg)
-// 	if err != nil {
-// 		return nil, err
-// 	}
+// SendEachForMulticastDryRun sends the given multicast message to all the specified FCM registration
+// tokens in the dry run (validation only) mode.
+func (c *Client) SendMulticastDryRun(ctx context.Context, message *messaging.MulticastMessage) (*messaging.BatchResponse, error) {
+	resp, err := c.client.SendEachForMulticastDryRun(ctx, message)
+	if err != nil {
+		return nil, err
+	}
 
-// 	resp := new(Response)
-// 	err = retry(func() error {
-// 		ctx, cancel := context.WithTimeout(ctx, c.timeout)
-// 		defer cancel()
-// 		var er error
-// 		resp, er = c.send(ctx, data)
-// 		return er
-// 	}, retryAttempts)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-
-// 	return resp, nil
-// }
-
-// // send sends a request.
-// func (c *Client) send(ctx context.Context, data []byte) (*Response, error) {
-// 	// create request
-// 	req, err := http.NewRequest("POST", c.endpoint, bytes.NewBuffer(data))
-// 	if err != nil {
-// 		return nil, err
-// 	}
-
-// 	req = req.WithContext(ctx)
-
-// 	// add headers
-// 	req.Header.Add("Authorization", fmt.Sprintf("key=%s", c.apiKey))
-// 	req.Header.Add("Content-Type", "application/json")
-
-// 	// execute request
-// 	resp, err := c.client.Do(req)
-// 	if err != nil {
-// 		return nil, connectionError(err.Error())
-// 	}
-// 	defer resp.Body.Close()
-
-// 	// check response status
-// 	if resp.StatusCode != http.StatusOK {
-// 		if resp.StatusCode >= http.StatusInternalServerError {
-// 			return nil, serverError(fmt.Sprintf("%d error: %s", resp.StatusCode, resp.Status))
-// 		}
-// 		return nil, fmt.Errorf("%d error: %s", resp.StatusCode, resp.Status)
-// 	}
-
-// 	// build return
-// 	response := new(Response)
-// 	if err := json.NewDecoder(resp.Body).Decode(response); err != nil {
-// 		return nil, err
-// 	}
-
-// 	return response, nil
-// }
+	return resp, nil
+}
